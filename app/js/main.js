5570fe16ae82a2abbb56ef038137424f0b25e5d5
Tarrence van As <tarrence13@gmail.com>
2013-11-11 18:01:50 -0500

    canvas fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,28 @@
 function init() {
     canvas = document.createElement('canvas');
 
-    canvas.width = window.innerWidth;
+    canvas.width = window.innerWidth - 300;
     canvas.height = window.innerHeight;
-    CONSTANTS.SCREENWITH = window.innerWidth;
+    CONSTANTS.SCREENWITH = window.innerWidth - 300;
     CONSTANTS.SCREENHEIGHT = window.innerHeight;
 
     canvas.setAttribute('id', 'c');
 
     document.body.appendChild(canvas);
-    stage = new Stage(canvas);
+    stage = new createjs.Stage(canvas);
     stage.mouseEventsEnabled = true;
 
     if(!createjs.Ticker.hasEventListener('tick')) {
         createjs.Ticker.addEventListener('tick', tick);
     }
     createjs.Ticker.setFPS(30);
-    createjs.Ticker.addListener(stage);
+
+    var doorObject = new DoorObject();
 }
 
 document.body.onload = init();
 
 function tick() {
     console.log('tick');
+    stage.update();
 }
