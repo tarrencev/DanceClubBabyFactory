67b4f9a9b3a23af75655f0d1d2dc3d83604ceaaf
Tarrence van As <tarrence13@gmail.com>
2013-11-14 15:06:57 -0500

    Changed web.js to server.js, switched port to 8080 for EC2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 app.use(express.static(__dirname + '/app'));
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
