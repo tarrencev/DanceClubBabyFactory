87c6075c79322007679ef9ba6a99f6918a867e0f
Tarrence van As <tarrence13@gmail.com>
2013-11-15 12:46:21 -0500

    Added Grunt. Run 'npm install' to get the new dependencies then use 'grunt' to run the node server' ;

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['app/js/*.js'],
+      // all: ['Gruntfile.js', 'app/js/*.js']
+    },
+    express: {
+      options: {
+        // Override defaults here
+        port: 8080
+      },
+      dev: {
+        options: {
+          script: 'server.js'
+        }
+      }
+    },
+    watch: {
+      express: {
+        files:  [ 'app/js/*.js' ],
+        tasks:  [ 'jshint', 'express:dev' ],
+        options: {
+          nospawn: true //Without this option specified express won't be reloaded
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'express:dev', 'watch']);
+};
